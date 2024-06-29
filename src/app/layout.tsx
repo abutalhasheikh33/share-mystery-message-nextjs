3f956c5aa31f4884d03b1eb4ced71dab0472314b
abutalhasheikh33 <abutalhasheikh33@gmail.com>
2024-06-30 00:59:45 +0530

    shardcn-setup/signup-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "@/components/ui/toaster"
 import AuthProvider from "@/context/AuthContext";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+        </body>
       </AuthProvider>
       
       
